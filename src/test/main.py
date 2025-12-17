bc6dd0fff7f480e508447cc25b76febc0c78345f
Ik <Ikkun19_touhou@proton.me>
2025-12-17 21:55:34 +0900

    Move variables first.

--- src/test/main.py
+++ src/test/main.py
@@ -1,11 +1,6 @@
 # Initialization, variables
 import pygame
 
-pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Shooting Game")
-clock = pygame.time.Clock()
-
 WIDTH = 480
 HEIGHT = 640
 WHITE = (255, 255, 255)
@@ -13,3 +8,8 @@ RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 BLACK = (0, 0, 0)
 GREEN = (0, 255, 0)
+
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Shooting Game")
+clock = pygame.time.Clock()
